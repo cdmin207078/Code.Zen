116446f73a7224a28f1087050bc5e310d78e66de
T410 <1372479001@qq.com>
2015-11-01 00:58:36 +0800

    初始化

---
+++ Code.Zen.Test/Dapper学习.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Code.Zen.Test
+{
+    [TestClass]
+    public class Dapper学习
+    {
+        [TestMethod]
+        public void 查询_返回单个对象()
+        {
+            Console.WriteLine("hello dapper.");
+        }
+    }
+}
